efb56a516c3d012bcbd339fc560eacd751c3243f
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-17 16:45:07 +0100

    test: case of password containing 9 characters

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -2,7 +2,7 @@ import { Main } from '../src/passwordChecker.js';
 
 describe('tests for a Password Checker', function() {
     const main = new Main();
-    
+
     it('should return false if the password contains 0 character', function() {
        
         const result = main.passwordChecker('');
@@ -13,6 +13,13 @@ describe('tests for a Password Checker', function() {
     it('should return true if the password contains 8 characters', function() {
 
         const result = main.passwordChecker('P@ssw0or');
+
+        expect(result).toBe(true);
+    });
+
+    it('should return true if the password contains 9 characters', function() {
+
+        const result = main.passwordChecker('P@ssw0ord');
 
         expect(result).toBe(true);
     });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,7 +1,7 @@
 export class Main {
     passwordChecker(password) {
-        if(password.length === 8) return true;
-        
+        if(password.length === 8 || password.length === 9) return true;
+
        return false;
     }
 }
