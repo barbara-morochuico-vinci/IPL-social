2946d0dddcf4d082e564ba1a4a5df18b6b4a072c
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-17 16:40:39 +0100

    test: case of password containing 8 characters

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -4,11 +4,22 @@ describe('tests for a Password Checker', function() {
     it('should return false if the password contains 0 character', function() {
         //arrange
         const main = new Main();
-        
+
         //act
         const result = main.passwordChecker('');
 
         //assert
         expect(result).toBe(false);
+    });
+
+    it('should return true if the password contains 8 characters', function() {
+        //arrange
+        const main = new Main();
+        
+        //act
+        const result = main.passwordChecker('P@ssw0or');
+
+        //assert
+        expect(result).toBe(true);
     });
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,7 @@
 export class Main {
     passwordChecker(password) {
+        if(password.length === 8) return true;
+        
        return false;
     }
 }
