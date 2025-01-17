7b368ad55e776664638879c5e9944a2047ceeab6
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-17 17:11:01 +0100

    test: case of password containing ipl

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -3,52 +3,60 @@ import { Main } from '../src/passwordChecker.js';
 describe('tests for a Password Checker', function() {
     const main = new Main();
 
-    it('should return false if the password contains 0 character', function() {
+    it('the function should return false if the password contains 0 character', function() {
        
         const result = main.passwordChecker('');
 
         expect(result).toBe(false);
     });
 
-    it('should return true if the password contains 8 characters', function() {
+    it('the function should return true if the password contains 8 characters', function() {
 
         const result = main.passwordChecker('P@ssw0or');
 
         expect(result).toBe(true);
     });
 
-    it('should return true if the password contains 9 characters', function() {
+    it('the function should return true if the password contains 9 characters', function() {
 
         const result = main.passwordChecker('P@ssw0ord');
 
         expect(result).toBe(true);
     });
 
-    it('should return true if the password contains 10 characters', function() {
+    it('the function should return true if the password contains 10 characters', function() {
 
         const result = main.passwordChecker('P@ssw0ordd');
 
         expect(result).toBe(true);
     });
 
-    it('should return true if the password contains the number 0 and is longer than 8 characters', function() {
+    it('the function should return true if the password contains the number 0 and is longer than 8 characters', function() {
 
         const result = main.passwordChecker('P@sswoo0rdd');
 
         expect(result).toBe(true);
     });
 
-    it('should return true if the password contains the number 1 and is longer than 8 characters', function() {
+    it('the function should return true if the password contains the number 1 and is longer than 8 characters', function() {
 
         const result = main.passwordChecker('P@sswoo1rdd');
 
         expect(result).toBe(true);
     });
 
-    it('should return true if the password contains the number 2 and is longer than 8 characters', function() {
+    it('the function should return true if the password contains the number 2 and is longer than 8 characters', function() {
 
         const result = main.passwordChecker('P@sswoo2rdd');
 
         expect(result).toBe(true);
     });
+
+    it('the function should return false if the password contains ipl and meets the other criteria', function() {
+
+        const result = main.passwordChecker('P@ssw_ipl_oo2rdd');
+
+        expect(result).toBe(false);
+    });
+
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,6 @@
 export class Main {
     passwordChecker(password) {
+        if(password.includes("ipl")) return false;
         if(isLongerThan8(password) && /\d/.test(password)) return true;
 
         return false;
