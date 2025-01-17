362c5e98ba51c5f8e2884d59ec049c72f228d2c9
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-17 16:34:52 +0100

    test: case of password containing 0 characters

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,4 +1,14 @@
+import { Main } from '../src/passwordChecker.js';
 
 describe('tests for a Password Checker', function() {
+    it('should return false if the password contains 0 character', function() {
+        //arrange
+        const main = new Main();
+        
+        //act
+        const result = main.passwordChecker('');
 
+        //assert
+        expect(result).toBe(false);
+    });
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,5 @@
 export class Main {
-    passwordChecker() {
-       
+    passwordChecker(password) {
+       return false;
     }
 }
