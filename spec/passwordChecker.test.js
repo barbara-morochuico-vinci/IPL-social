d1428b46d4005d982d7a6ce7629f2aaf84cb4197
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-17 16:44:10 +0100

    refactor: main moved to the beginning of the describe

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,25 +1,19 @@
 import { Main } from '../src/passwordChecker.js';
 
 describe('tests for a Password Checker', function() {
+    const main = new Main();
+    
     it('should return false if the password contains 0 character', function() {
-        //arrange
-        const main = new Main();
-
-        //act
+       
         const result = main.passwordChecker('');
 
-        //assert
         expect(result).toBe(false);
     });
 
     it('should return true if the password contains 8 characters', function() {
-        //arrange
-        const main = new Main();
-        
-        //act
+
         const result = main.passwordChecker('P@ssw0or');
 
-        //assert
         expect(result).toBe(true);
     });
 });
